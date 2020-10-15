eb0bb83fc8892aecec5a6428f18ba9d4c47f33c3
Vicki <vicki@h-iMac-sl-Vicki.local>
2020-10-15 19:58:51 +0300

    Updates

---
+++ precache-manifest.fd372f8df4a8eed9892a2b419edc0741.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9000d4c8da31bde91ea40640ce151485",
+    "url": "/GOL2/index.html"
+  },
+  {
+    "revision": "0bdd419b9eb191f8f5d9",
+    "url": "/GOL2/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4d92776239a717866700",
+    "url": "/GOL2/static/js/2.b9f20537.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/GOL2/static/js/2.b9f20537.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bdd419b9eb191f8f5d9",
+    "url": "/GOL2/static/js/main.832d7afb.chunk.js"
+  },
+  {
+    "revision": "26742f31dfd9d944cfdb",
+    "url": "/GOL2/static/js/runtime-main.befbc43f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.740e2a54dfff36d485477e925acbaa45.js"
+  "/GOL2/precache-manifest.fd372f8df4a8eed9892a2b419edc0741.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/GOL2/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
